2d398f5d53f37112c80ea0e718464aa973352e89
Vašek Dohnal <vaclav.dohnal@gmail.com>
2015-01-03 23:59:38 +0200

    New folder structure; Remove *.pyc files.

--- fabfile.py
+++ fabfile.py
@@ -28,7 +28,7 @@ env.hosts = [remote_section["host"]]
 env.user = remote_section["user"]
 env.key_filename = os.path.normpath(remote_section["key_filename"])
 
-APP_BASE_DIR = '/var/www/gtasksapp_com/www'
+APP_BASE_DIR = '/var/www/gtasksapp_com/www/app'
 DIST_ZIP_FILENAME = "dist.zip"
 DIST_DIRECTORY_NAME = "dist"
 
@@ -40,6 +40,7 @@ def deploy():
 
     create_zip_archive(dist_path, DIST_ZIP_FILENAME)
 
+    run("mkdir -p {0}".format(APP_BASE_DIR))
     put(DIST_ZIP_FILENAME, APP_BASE_DIR)
 
     with cd(APP_BASE_DIR):
@@ -47,3 +48,18 @@ def deploy():
         run("rm {0}".format(DIST_ZIP_FILENAME))
 
     os.remove(DIST_ZIP_FILENAME)
+
+@task(alias='l')
+def landing():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    dist_path = os.path.join(current_path, "landing_page")
+
+    create_zip_archive(dist_path, ".tmp/landing_page.zip")
+
+    put(".tmp/landing_page.zip", "/var/www/gtasksapp_com/www/")
+
+    with cd("/var/www/gtasksapp_com/www/"):
+        run("unzip -o {0}".format("landing_page.zip"))
+        run("rm {0}".format("landing_page.zip"))
+
+    os.remove(".tmp/landing_page.zip")
